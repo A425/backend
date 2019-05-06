4840420599a82b3da7d561236460e8f31be54d95
YuChen Liu <yuchen.liu@grabtaxi.com>
2019-05-06 23:44:57 +0800

    add jwt encode

--- authCenter/handler/wechatAuth.go
+++ authCenter/handler/wechatAuth.go
@@ -3,7 +3,9 @@ package handler
 import (
 	"context"
 	"fmt"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/micro/go-log"
 	"github.com/valyala/fasthttp"
 
@@ -22,10 +24,22 @@ func (a *WechatAuth) VerifyWechatCode(ctx context.Context, req *wechatAuth.Verif
 	log.Log("Received WechatAuth.VerifyWechatCode request:" + req.GetCode())
 	rsp.Msg = "Hello " + req.Code
 	url := fmt.Sprintf(WechatValidationEndpoint, "appid", "secret", req.Code)
+
+	// using github.com/json-iterator/go to unmarshal
 	st, resp, err := fasthttp.Get(nil, url)
 	if err != nil {
 		return err
 	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"foo": "bar",
+		"nbf": time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+	})
+
+	// Sign and get the complete encoded token as a string using the secret
+	tokenString, err := token.SignedString("hmacSampleSecret")
+
+	fmt.Println(tokenString, err)
 
 	log.Logf("st:%d, resp:%s", st, resp)
 	return nil
